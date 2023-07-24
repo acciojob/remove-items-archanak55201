ae6ac65b8169ef8b030954e8f715e427204ca2aa
archanak55201 <55021100+archanak55201@users.noreply.github.com>
2023-07-25 00:24:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+  const buttonre= document.querySelector("input[type=button]")
+    const select= document.getElementById("colorSelect").children;
+    buttonre.addEventListener("click",()=>{
+       for(let i=0;i<select.length;i++){
+                if(select[i].selected){
+                    select[i].remove();
+                }
+        }
+    })
